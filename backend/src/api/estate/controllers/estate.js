9c740ef925ef3e51d8fab15639acb6b731b8e03d
BonhaengLee <bon952014@ajou.ac.kr>
2022-03-25 03:56:19 +0900

    [Feat] render homePage

--- backend/src/api/estate/controllers/estate.js
+++ backend/src/api/estate/controllers/estate.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  *  estate controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::estate.estate');
+module.exports = createCoreController("api::estate.estate");
